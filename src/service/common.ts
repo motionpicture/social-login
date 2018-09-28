52ea7dd4708e93bb31cc1273529159c163b30ab7
thanhBriswell <thanh.clg@briswell.com>
2018-09-28 13:26:04 +0700

    MWSR-213 ソーシャルプラス連携

--- src/service/common.ts
+++ src/service/common.ts
@@ -1,42 +1,23 @@
 /**
  * check null and return null or date
  */
-function checkForDate(data: any): any {
-    let result: any = '';
-    try {
-        if (data !== null && data !== undefined) {
-            const timestamp = Date.parse(data);
-            if (isNaN(timestamp) === false) {
-                result = new Date(timestamp);
-            } else {
-                result = undefined;
-            }
+function checkForDate(checker: any, data: any): any {
+    if (checker !== null) {
+        if (data !== null) {
+            return new Date(data);
         } else {
-            result = undefined;
+            return undefined;
         }
-    } catch (err) {
-        result = undefined;
+    } else {
+        return undefined;
     }
-
-    return result;
 }
 
 /**
  * check null
  */
-function checkNull(data: any): any {
-    let result: any = '';
-    try {
-        if (data !== null && data !== undefined) {
-            result = data;
-        } else {
-            result = undefined;
-        }
-    } catch (err) {
-        result = undefined;
-    }
-
-    return result;
+function checkNull(checker: any, data: any): any {
+    return (checker !== undefined) ? data : undefined;
 }
 
 export { checkForDate, checkNull };
